cc6aec3ff207d5954d0a7bb0845ad8149b07a1c1
RyeonDeveloper <luvlik207@gmail.com>
2021-12-10 11:27:19 +0900

    database 연동

---
+++ database/add_data.sql
@@ -0,0 +1,12 @@
+-- desc store;
+
+-- insert into Store values ("000-00-000", "admin", "카페다락", "김련", "010-0000-0000");
+-- insert into Store values ("111-11-111", "admin", "카페바", "련경", "010-0000-1111");
+  
+-- select * from store;
+
+-- 여기서부터 하자
+-- insert into user values ("myidis00", "password123", "김손님", "010-1111-0000");
+-- insert into user values ("myidis01", "password123", "고손님", "010-1111-1111");
+-- insert into user values("myidis02", "password123", "이손님", "010-1111-2222");
+
--- paceweb/paceweb/settings.py
+++ paceweb/paceweb/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import my_settings
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -88,12 +89,8 @@ WSGI_APPLICATION = 'paceweb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = my_settings.DATABASES
+SECRET_KEY = my_settings.SECRET_KEY
 
 
 # Password validation
